f1b425edc38087c630481f181960a2a7d68ff9fd
Thomas Mutton <tmutton@spacehive.com>
2019-04-24 16:34:29 +0100

    - now converting pdf files to images

--- PdfToImageTest/Program.cs
+++ PdfToImageTest/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PDFiumSharp;
+using System;
+using System.IO;
 
 namespace PdfToImageTest
 {
@@ -6,7 +8,25 @@ namespace PdfToImageTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Converting pdf to image..");
+
+            using (var doc = new PdfDocument("c:\\test.pdf"))
+            {
+                int i = 0;
+                foreach (var page in doc.Pages)
+                {
+                    using (var bitmap = new PDFiumBitmap((int)page.Width, (int)page.Height, true))
+                    using (var stream = new FileStream($"{i++}.bmp", FileMode.Create))
+                    {
+                        page.Render(bitmap);
+                        bitmap.Save(stream);
+                    }
+                }
+            }
+
+            Console.WriteLine("Finished converting pdf to image..");
+
+            Console.Read();
         }
     }
 }
